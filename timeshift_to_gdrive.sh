a8b0fb7b41e493276cee9859405b59d7566acaf0
BitsAndAtoms <siddhartha.che@gmail.com>
2023-07-03 01:59:52 -0600

    webversion untested

--- timeshift_to_gdrive.sh
+++ timeshift_to_gdrive.sh
@@ -11,10 +11,13 @@ LATEST_SNAPSHOT=$(ls -Art $TIMESHIFT_DIR | tail -n 1)
 OLDEST_SNAPSHOT=$(ls -Art $TIMESHIFT_DIR | head -n 1)
 mkdir -p $BACKUP_DIR
 tar -czf "$BACKUP_DIR/latestbckp.tar.gz" -C "$TIMESHIFT_DIR/$LATEST_SNAPSHOT" . -v
-tar -czf "$BACKUP_DIR/oldestbckp.tar.gz" -C "$TIMESHIFT_DIR/$OLDEST_SNAPSHOT" . -v
-
-getent group bckupgrp >/dev/null || /usr/sbin/groupadd bckupgrp
-id -nG "$USER" | grep -qw bckupgrp || /usr/sbin/usermod -aG bckupgrp "$USER"
-
-chown -R :bckupgrp $BACKUP_DIR
-chmod -R 770 $BACKUP_DIR
+. /root/mysecretsforbackupjobs
+TOKEN_RESPONSE=$(curl --request POST --data "client_id=${CLIENT_ID_BACKUP}&client_secret=${CLIENT_SECRET_BACKUP}&refresh_token=${REFRESH_TOKEN_BACKUP}&grant_type=refresh_token" https://accounts.google.com/o/oauth2/token)
+ACCESS_TOKEN=$(echo $TOKEN_RESPONSE | jq -r .access_token)
+# Replace FILE_ID with the ID of the file you want to update
+curl --request PUT \
+  --header "Authorization: Bearer $ACCESS_TOKEN" \
+  --header "Content-Type: application/gzip" \
+  --data-binary @"$BACKUP_DIR/latestbckp.tar.gz" \
+  "https://www.googleapis.com/upload/drive/v3/files/?uploadType=media"
+# tar -czf "$BACKUP_DIR/oldestbckp.tar.gz" -C "$TIMESHIFT_DIR/$OLDEST_SNAPSHOT" . -v
